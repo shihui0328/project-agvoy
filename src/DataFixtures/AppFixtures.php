04f3ece88f9b2972ef8f28bc668306d47f3a057a
shihui0328 <huangshihui0328@outlook.com>
2019-10-05 17:18:35 +0200

    Created first fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,12 +5,38 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
+
 class AppFixtures extends Fixture
 {
+
+  public const IDF_REGION_REFERENCE = 'idf-region';
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $region = new Region();
+    $region->setCountry("FR");
+    $region->setName("Ile de France");
+    $region->setPresentation("La région française capitale");
+    $manager->persist($region);
+
+    $manager->flush();
+    // Une fois l'instance de Region sauvée en base de données,
+    // elle dispose d'un identifiant généré par Doctrine, et peut
+    // donc être sauvegardée comme future référence.
+    $this->addReference(self::IDF_REGION_REFERENCE, $region);
+
+    // $product = new Product();
+    // $manager->persist($product);
+
+    $room = new Room();
+    $room->setSummary("Beau poulailler ancien à Évry");
+    $room->setDescription("très joli espace sur paille");
+    //$room->addRegion($region);
+    // On peut plutôt faire une référence explicite à la référence
+    // enregistrée précédamment, ce qui permet d'éviter de se
+    // tromper d'instance de Region :
+    $room->addRegion($this->getReference(self::IDF_REGION_REFERENCE));
+    $manager->persist($room);
 
         $manager->flush();
     }
